86ac60f5be4c872858dea9005bbf91142c82172c
raresJM <rares.scarlat.jm@gmail.com>
2017-02-21 00:51:55 +0200

    added Taxi problem

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TarifTaxi
+{
+    [TestClass]
+    public class TarifTaxi
+    {
+        [TestMethod]
+        public void TestCalculateTaxiRate_DayTime_5km()
+        {
+            Assert.AreEqual(5,10);
+        }
+    }
+}
