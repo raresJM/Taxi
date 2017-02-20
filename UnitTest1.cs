f1d6483761542a405b8643ec57342676232e3aac
raresJM <rares.scarlat.jm@gmail.com>
2017-02-21 01:02:06 +0200

    added calculateTaxiRate()

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,19 +9,41 @@ namespace TarifTaxi
         [TestMethod]
         public void TestCalculateTaxiRate_DayTime_5km()
         {
-            Assert.AreEqual(2, calculateTaxiRate(5,10);
+            Assert.AreEqual(2, calculateTaxiRate(5,10));
         }
 
         [TestMethod]
         public void TestCalculateTaxiRate_DayTime_21km()
         {
-            Assert.AreEqual(168, calculateTaxiRate(21, 10);
+            Assert.AreEqual(168, calculateTaxiRate(21, 10));
         }
 
         [TestMethod]
         public void TestCalculateTaxiRate_DayTime_65km()
         {
-            Assert.AreEqual(390, calculateTaxiRate(65, 10);
+            Assert.AreEqual(390, calculateTaxiRate(65, 10));
+        }
+
+
+        public double calculateTaxiRate(double distance, double hour)
+        {
+            double realUnitFee = 0;
+            if(distance <= 20)
+            {
+                realUnitFee = 5;
+            }
+
+            if (distance >= 21 && distance < 60)
+            {
+                realUnitFee = 4*2;
+            }
+
+            if (distance > 60)
+            {
+                realUnitFee = 3*2;
+            }
+
+            return Math.Round((distance * realUnitFee), 2);
         }
 
 
